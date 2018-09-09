f6bc15cedcd5812051508d0ec2cfd258e59cd026
Damian Arquez <damian.arquez@ing.uchile.cl>
2018-09-09 13:08:59 -0300

    Add voting

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -4,4 +4,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   field :createLink, function: Resolvers::CreateLink.new
   field :createUser, function: Resolvers::CreateUser.new
   field :signInUser, function: Resolvers::SignInUser.new
+  field :voteLink, function: Resolvers::CreateVote.new
 end
